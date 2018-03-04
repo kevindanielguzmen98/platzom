aea772029b60a5a0cdda5a04a868b7271384aba5
kevocode <kevindanielguzmen98@gmail.com>
2018-03-03 19:15:21 -0500

    Creación de dependencias y configuracion de transpilador

---
+++ dist/platzom.js
@@ -0,0 +1,43 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = platzom;
+function platzom(str) {
+    var translation = str;
+    /* Si la palabra termina con "ar" se le quitan esas dos letras */
+    if (str.toLowerCase().endsWith('ar')) {
+        translation = str.slice(0, -2);
+    }
+    /* Si la palabra inicia con Z se le añade "pe" al final */
+    if (str.toLowerCase().startsWith('z')) {
+        translation += 'pe';
+    }
+    /* Si la palabra traducida tiene 10 o más letras se debe partir en dos por la mitad */
+    var length = translation.length;
+    if (length >= 0) {
+        var firstHalf = translation.slice(0, Math.round(length / 2));
+        var secondHalf = translation.slice(Math.round(length / 2));
+        translation = firstHalf + '-' + secondHalf;
+    }
+    /*  */
+    if (str == reverse(str)) {
+        return minMay(str);
+    }
+}
+
+function minMay(str) {
+    var translation = '';
+    var capitalize = true;
+    for (var i = 0; i < str.length; i++) {
+        var char = str.charAt(i);
+        translation += capitalize ? char.toUpperCase() : char.toLowerCase();
+        capitalize = !capitalize;
+    }
+    return translation;
+}
+
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
